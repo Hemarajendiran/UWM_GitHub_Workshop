d0587e91b4caaa2a7d84f5cf892796debe309686
HemaRajendiran <hemaraj.nh@gmail.com>
2020-12-10 17:37:37 -0600

    COMMIT MSGS

--- python_scripts/script_2.py
+++ python_scripts/script_2.py
@@ -12,32 +12,32 @@
 '''
 
 
-def drawW():
-	'''Draw the W body'''
-	# read in data
-	W = pd.read_csv('data/letter_2.csv',
-					header = None)
-	x, y = list(W[0]), list(W[1])
-	x.append(x[0])
-	y.append(y[0])
+#def drawW():
+#	'''Draw the W body'''
+#	# read in data
+#	W = pd.read_csv('data/letter_2.csv',
+#					header = None)
+#	x, y = list(W[0]), list(W[1])
+#	x.append(x[0])
+#	y.append(y[0])
 
-	# plot data
-	ax.fill(x, y, color = 'black')
-	ax.plot(x, y, color = 'black', linewidth = 2)
+#	# plot data
+#	ax.fill(x, y, color = 'black')
+#	ax.plot(x, y, color = 'black', linewidth = 2)
 
 ## Solution ----
-# def drawW():
-# 	'''Draw the W body'''
-# 	# read in data
-# 	W = pd.read_csv('data/letter_2.csv',
-# 					header = None)
-# 	x, y = list(W[0]), list(W[1])
-# 	x.append(x[0])
-# 	y.append(y[0])
-#
-# 	# plot data
-# 	ax.fill(x, y, color = (0, 0.2, 0.4))  # per reviewer comment
-# 	ax.plot(x, y, color = 'black', linewidth = 2)
+ def drawW():
+ 	'''Draw the W body'''
+ 	# read in data
+ 	W = pd.read_csv('data/letter_2.csv',
+ 					header = None)
+ 	x, y = list(W[0]), list(W[1])
+ 	x.append(x[0])
+ 	y.append(y[0])
+
+ 	# plot data
+ 	ax.fill(x, y, color = (0, 0.2, 0.4))  # per reviewer comment
+ 	ax.plot(x, y, color = 'black', linewidth = 2)
 
 
 # You will not need to edit the code below
